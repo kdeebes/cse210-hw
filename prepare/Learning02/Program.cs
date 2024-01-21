31542a29c1b4582b23bffda1885d07125f8e2003
kdeebes <deebes.kerry21@gmail.com>
2024-01-21 09:54:37 -0400

    completed assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Creates a new job instance named job1
+        Job job1 = new Job();
+
+        // Set member variables using dot notation
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Creates a second job instance
+        Job job2 = new Job();
+
+        // Set member variables for job2
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Kerryann Deebes";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
